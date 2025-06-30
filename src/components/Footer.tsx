dff5775da4f5a4319418ed622d7d08044819045e
YashNagrale <ynagrale668@gmail.com>
2025-06-30 10:53:31 +0530

    Footer removed

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-//TODO: Add star to repo at footer
-
-function Footer() {
-  return (
-    <footer className="bg-foreground text-muted-foreground p-4">Footer</footer>
-  );
-}
-
-export default Footer;
