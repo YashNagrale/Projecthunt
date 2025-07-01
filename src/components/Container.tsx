fff3975ecfc29e5afde22e8bab5e559b7d2d82d9
YashNagrale <ynagrale668@gmail.com>
2025-07-01 21:42:47 +0530

    container update

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 type ChildrenProp = {
   children: React.ReactNode;
+  className?: string;
 };
-function Container({ children }: ChildrenProp) {
-  return <div className="w-full mx-auto p-2">{children}</div>;
+function Container({ children, className }: ChildrenProp) {
+  return <div className={`w-full mx-auto p-2 ${className}`}>{children}</div>;
 }
 
 export default Container;
